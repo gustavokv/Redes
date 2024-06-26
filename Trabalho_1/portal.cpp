16aefa32b48ecfb3d0386cfcf61525d7645b7995
Gustavo Volobueff <volobueffgustavo@gmail.com>
2024-06-26 18:09:47 -0400

    a

--- Trabalho_1/portal.cpp
+++ Trabalho_1/portal.cpp
@@ -48,7 +48,11 @@ void *recebe_arquivos_fonte(void *meu_socket){
 	//EnderecoHandler addrServidores[3] = {EnderecoHandler(ip_maquina_1_aqui, 18900), EnderecoHandler(ip_maquina_2_aqui, 18901), EnderecoHandler(ip_maquina_3_aqui, 18902)};
 
 	meu_socket_server = socket(AF_INET, SOCK_STREAM, 0);
+
 	addrServidores.bindarComSocket(meu_socket_server);
+	// addrServidores[0].bindarComSocket(meu_socket_server);
+	// addrServidores[1].bindarComSocket(meu_socket_server);
+	// addrServidores[2].bindarComSocket(meu_socket_server);
 
 	if(connect(meu_socket_server, (struct sockaddr*)addrServidores.getAddrAddr(), sizeof(addrServidores.getAddr())) == -1){
         cout << "Erro em se conectar ao servidor." << endl;
